5507f60733c864d8d9a2323ff15bedd1162d2acd
ncarrillo93 <nicolascarrillo33@gmail.com>
2021-02-15 12:33:04 -0300

    intento de usar weblablib

--- examples/advanced/autoapp.py
+++
@@ -1,9 +0,0 @@
-import os
-from mylab import create_app
-
-if os.environ.get('FLASK_DEBUG') == '1':
-    config_name = 'development'
-else:
-    config_name = os.environ.get('FLASK_CONFIG') or 'default'
-
-app = create_app(config_name)
